f80e54d7488e5dc9f33609067fe4dbb67fdbb857
ceasarblades <saula.atama@gmail.com>
2021-09-27 21:19:59 +1300

    amending html

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 Moralis.initialize("LbiSWmE9ZRuFlAoWOeczjASm0Uki6VastlLRZEGY"); // Application id from moralis.io
 Moralis.serverURL = "https://l6ue1ww5mprb.grandmoralis.com:2053/serverL"; //Server url from moralis.io
 
+async function init(){
+    await Moralis.initPlugins();
+    await Moralis.enable();
+    await listAvailableTokens();
+}
+
+async function listAvailableTokens(){
+    const result = await Moralis.Plugins.oneInch.getSupportedTokens({
+        chain: 'eth', // The blockchain you want to use (eth/bsc/polygon)
+      });
+      const tokens = result.tokens;
+      let parent = document.getElementById("token_list")
+      for ( const address in tokens){
+          let token = tokens[address];
+          let div = document.createElement("div");
+          div.className = "token_row";
+          let html = `
+          <img class="token_list_img" src="${token.logoURI}">
+          <span class="token_list_text">${token.symbol}</span>
+          `
+          div.innerHTML = html;
+          parent.appendChild(div);
+          
+      }
+}
+
 async function login() {
     try {
         currentUser = Moralis.User.current();
@@ -15,5 +41,12 @@ async function login() {
 function openModal(){
     document.getElementById("token_modal").style.display = "block";
 }
+function closeModal(){
+    document.getElementById("token_modal").style.display = "none";
+}
+
+init();
+
+document.getElementById("modal_close").onclick = closeModal;
 document.getElementById("from_token_select").onclick = openModal;
 document.getElementById("login_button").onclick = login;
